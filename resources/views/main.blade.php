ffa89e6512850c33cba05310d1faf85660bc5516
tonggan198699 <gantong1986@gmail.com>
2018-02-01 22:18:50 +0000

    Adding boilerplate

---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html ng-app="myApp">
+<head>
+  <meta charset="utf-8">
+  <title>Import AngularJS to Laravel Project</title>
+  <meta name="viewport" content="width=device-width">
+
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular.min.js"></script>
+    <script src="js/main.js"></script>
+    <h1>fsdfds</h1>
+</body>
+
+</html>
